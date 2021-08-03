1a6f92a5794dd4265993f88aa94466d10ee2d056
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-08-03 11:50:21 +1000

    Added Text Change Event for Worksheet Titles
    Generally cleaned up code, changed some fonts and made XAML more readable/useable

--- ChineseGame/ChineseGame/MainWindow.xaml.cs
+++ ChineseGame/ChineseGame/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace ChineseGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        //SETUP--------------------------------------------------
+
+        //Declare variables
+        //Titles
+        private string SheetTitle = "";
+        private string SheetTitleChinese = "";
+        //Grid
+        private int GridSize = 6;
+        private string[,] GridData;
+        private bool GridGenned = false;
+        //Word data
+        private List<string[]> WordData;
+        private int MaxWords;
+
+        //Constructor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //INPUTS-----------------------------------------------
+
+        //Update titles
+        public void UpdateTitles()
+        {
+            SheetTitle = WorksheetTitleInput.Text;
+            SheetTitleChinese = WorksheetTitleChineseInput.Text;
+        }
+        
+        //Update titles overload (for form elements)
+        public void UpdateTitles(object sender, TextChangedEventArgs e)
+        {
+            SheetTitle = WorksheetTitleInput.Text;
+            SheetTitleChinese = WorksheetTitleChineseInput.Text;
         }
     }
 }
