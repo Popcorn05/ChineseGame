8bd4e77d9e37813c8dd4243e0149f98ed00b5cc2
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-08-09 09:47:25 +1000

    Squashed commit of the following:
    commit 61fda52b524485cf2e831e9c8ae8a287442e4a0b
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 13:49:29 2021 +1000
    
        Displays Save Window
    
        Displays save window when save button in editor clicked, no functionality on save window yet
    
    commit c90e506dc6ca7cd85bab6f3052ca169d5c48adcd
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 13:43:03 2021 +1000
    
        Fixed Merge Issue
    
        Started Save Window instead of open, holdover from save window design
    
    commit f05f21ce7314d34136421d540b169760013f03fc
    Merge: c321172 8f0e412
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 13:41:17 2021 +1000
    
        Merge branch 'daniel' into dev
    
    commit c321172e3c8817a0aef85093889c987005786f85
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 13:36:59 2021 +1000
    
        Changed Deletion Slightly
    
    commit 4f00ef4ece8fd61b7b60d6db7816cc5e56d6d0c7
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 13:23:27 2021 +1000
    
        Added Delete Row Button
    
        Save button implemented, not functioning
    
    commit a46b18810fbe2d30f3868e1518a9a2298742e293
    Author: Popcorn05 <flynn@rewill.net>
    Date:   Sun Aug 8 09:28:37 2021 +1000
    
        Added Expanding WordData input
    
        Implemented table for inputting word data. Expands when bottom row entered into. No delete row button yet.
    
    commit 402848ff9cf4f048892d2c3fdfa7bc578675b170
    Author: Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
    Date:   Thu Aug 5 11:53:42 2021 +1000
    
        Styled Slider and GridSize label, Added Generate Button (No Function)
    
        No generate function added, no word data to act as input
    
    commit bf6e3181d40b851bfaf481cef773f4dc26e9ef4e
    Author: Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
    Date:   Thu Aug 5 09:08:03 2021 +1000
    
        Fixed Grid Outer Border Issue
    
        Made grid inside larger border to account for double ups on inside cell borders
    
    commit 4f9c2a20eddf03174d326f9200f56a19912ef2e8
    Author: Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
    Date:   Wed Aug 4 14:40:02 2021 +1000
    
        Made Grid Visible, Thickness needs fixing
    
        Grid now visible and changes with slider, however thickness of inside parts is higher due to double up on edges. Needs fixing
    
    commit 69f2a62edec5433fd2865106e7809a243f44b4e7
    Author: Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
    Date:   Tue Aug 3 15:15:05 2021 +1000
    
        Added Slider and Grid Creation (Not Visible)
    
        No testing done on grid creation, no idea if it works.

--- ChineseGame/ChineseGame/MainWindow.xaml.cs
+++ ChineseGame/ChineseGame/MainWindow.xaml.cs
@@ -30,14 +30,54 @@ namespace ChineseGame
         private int GridSize = 6;
         private string[,] GridData;
         private bool GridGenned = false;
+        const double GridSizePixels = 275.0;
+        private Grid PreviewGrid;
+        private Border GridBorder;
+        private List<Border> GridBorders;
         //Word data
         private List<string[]> WordData;
+        private List<TextBox[]> WordDataObjects;
+        private List<Button> WordDataButtons;
         private int MaxWords;
 
         //Constructor
         public MainWindow()
         {
+            //Init window
             InitializeComponent();
+            
+            //Create start grid
+            PreviewGrid = new Grid();
+
+            //Add styles
+            PreviewGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            PreviewGrid.VerticalAlignment = VerticalAlignment.Center;
+
+            //Grid border
+            GridBorder = new Border();
+            GridBorder.BorderBrush = Brushes.Black;
+            GridBorder.BorderThickness = new Thickness(4);
+            GridBorder.Width = GridSizePixels;
+            GridBorder.Height = GridSizePixels;
+            GridBorder.Child = PreviewGrid;
+
+            GridBorders = new List<Border>();
+
+            //Add to body grid
+            BodyGrid.Children.Add(GridBorder);
+            Grid.SetColumn(GridBorder, 1);
+            Grid.SetRow(GridBorder, 2);
+
+            //Update grid size
+            UpdateGridSize();
+
+            //Instantiate word data lists
+            WordData = new List<string[]>();
+            WordDataObjects = new List<TextBox[]>();
+            WordDataButtons = new List<Button>();
+
+            //Word data first row
+            AddWordDataRow();
         }
 
         //INPUTS-----------------------------------------------
@@ -54,6 +94,149 @@ namespace ChineseGame
         {
             SheetTitle = WorksheetTitleInput.Text;
             SheetTitleChinese = WorksheetTitleChineseInput.Text;
+        }
+
+        //Update Grid Size
+        public void UpdateGridSize()
+        {
+            //Clear current
+            PreviewGrid.RowDefinitions.Clear();
+            PreviewGrid.ColumnDefinitions.Clear();
+            PreviewGrid.Children.Clear();
+            GridBorders.Clear();
+
+            //Add rows and columns
+            RowDefinition PreviewRow = new RowDefinition();
+            PreviewRow.Height = new GridLength(Math.Round(GridSizePixels / GridSize));
+            ColumnDefinition PreviewColumn = new ColumnDefinition();
+            PreviewColumn.Width = new GridLength(Math.Round(GridSizePixels / GridSize));
+
+            //Loop through to add rows and columns
+            for (int p = 0; p < GridSize; p++)
+            {
+                PreviewGrid.RowDefinitions.Add(new RowDefinition() { Height = PreviewRow.Height });
+                PreviewGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = PreviewColumn.Width });
+            }
+
+            //Loop through and add borders
+            int i = 0;
+            for (int y = 0; y < GridSize; y++)
+            {
+                for (int x = 0; x < GridSize; x++)
+                {
+                    GridBorders.Add(new Border { BorderBrush = GridBorder.BorderBrush, BorderThickness = new Thickness(2) });
+                    PreviewGrid.Children.Add(GridBorders[i]);
+                    Grid.SetColumn(GridBorders[i], x);
+                    Grid.SetRow(GridBorders[i], y);
+                    i++;
+                }
+            }
+        }
+
+        //Update grid size overload for form elements
+        public void UpdateSlider(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //Change grid size
+            GridSize = (int)e.NewValue;
+            TestLabel.Content = "Grid Size: " + GridSize.ToString();
+            //Only run update grid if preview grid exists
+            if (PreviewGrid != null)
+            {
+                UpdateGridSize();
+            }
+        }
+
+        //Update word data grid
+        public void WordDataUpdate(object sender, TextChangedEventArgs e)
+        {
+            //Get current row num
+            int WordRowNum = WordDataObjects.Count;
+
+            //If any textbox is filled in bottom row add row
+            if (WordDataObjects[WordRowNum-1][0].Text != "" || WordDataObjects[WordRowNum-1][1].Text != "" || WordDataObjects[WordRowNum-1][2].Text != "")
+            {
+                AddWordDataRow();
+                WordDataButtons[WordRowNum - 1].IsEnabled = true;
+            }
+        }
+
+        //Add row to word data grid
+        public void AddWordDataRow()
+        {
+            //Add new row def
+            WordDataGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(40.0) });
+
+            //Add row to objects list
+            WordDataObjects.Add(new TextBox[] { new TextBox(), new TextBox(), new TextBox() });
+
+            //Get current row num (w/ new row)
+            int WordRowNum = WordDataObjects.Count;
+
+            //Loop through and format textboxes
+            for (int i = 0; i < 3; i++)
+            {
+                WordDataObjects[WordRowNum - 1][i].TextChanged += new TextChangedEventHandler(WordDataUpdate);
+                WordDataGrid.Children.Add(WordDataObjects[WordRowNum-1][i]);
+                Grid.SetColumn(WordDataObjects[WordRowNum-1][i], i);
+                Grid.SetRow(WordDataObjects[WordRowNum-1][i],WordRowNum-1);
+            }
+
+            //Add button to list and grid and format
+            WordDataButtons.Add(new Button() { Content = "X" });
+            WordDataButtons[WordRowNum - 1].Click += new RoutedEventHandler(WordDataButtonClick);
+            WordDataGrid.Children.Add(WordDataButtons[WordRowNum-1]);
+            Grid.SetColumn(WordDataButtons[WordRowNum-1], 3);
+            Grid.SetRow(WordDataButtons[WordRowNum-1], WordRowNum-1);
+
+            //Always disable at first
+            WordDataButtons[WordRowNum - 1].IsEnabled = false;
+        }
+
+        //Remove row from word data grid
+        public void RemoveWordDataRow(int RowNum)
+        {
+            //Remove objects as grid children
+            for (int i = 0; i < 3; i++)
+            {
+                WordDataGrid.Children.Remove(WordDataObjects[RowNum][i]);
+            }
+            WordDataGrid.Children.Remove(WordDataButtons[RowNum]);
+
+            //Remove row
+            WordDataGrid.RowDefinitions.RemoveAt(RowNum);
+
+            //Remove objects from respective lists
+            WordDataObjects.RemoveAt(RowNum);
+            WordDataButtons.RemoveAt(RowNum);
+
+            //Update grid positions
+            for (int r = 0; r < WordDataObjects.Count(); r++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Grid.SetRow(WordDataObjects[r][i], r);
+                }
+                Grid.SetRow(WordDataButtons[r], r);
+            }
+        }
+
+        //Event handler for word data gridrow remove button
+        public void WordDataButtonClick(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < WordDataButtons.Count(); i++)
+            {
+                if (WordDataButtons[i] == sender)
+                {
+                    RemoveWordDataRow(i);
+                }
+            }
+        }
+
+        //Open save window
+        public void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            SaveWindow Save = new SaveWindow();
+            Save.Show();
         }
     }
 }
